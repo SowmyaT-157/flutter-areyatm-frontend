d959cc5217c2adc7ef539995087c7f5ec0146e60
sowmya <sowmya.t@everest.engineering>
2026-04-06 23:48:21 +0530

    Update the authentication service to add the register integration

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -34,3 +34,41 @@ Future<void> loginUser(
     ).showSnackBar(const SnackBar(content: Text('internet server error')));
   }
 }
+
+Future<void> createUser(
+  String firstName,
+  String lastName,
+  String email,
+  String password,
+  BuildContext context,
+) async {
+  final url = 'http://127.0.0.1:3007/user';
+  try {
+    final response = await http.post(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email,
+        'password': password,
+      }),
+    );
+    if (!context.mounted) {
+      return;
+    }
+    if (response.statusCode == 201) {
+      context.go('/login');
+    } else {
+      var data = jsonDecode(response.body);
+      print('User created: $data');
+    }
+  } catch (e) {
+    print("error comming: $e");
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text('internet server error')));
+  }
+}
