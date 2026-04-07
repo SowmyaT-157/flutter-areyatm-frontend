5936953142b3bfa007dc6e86823d1c3b301a95ca
sowmya <sowmya.t@everest.engineering>
2026-04-08 00:05:43 +0530

    Update the home screen to display the tasks in the screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,102 @@
+import 'package:arey_atm/model/taskModel.dart';
+import 'package:arey_atm/services/tasksServices.dart';
 import 'package:flutter/material.dart';
 
-class homePage extends StatefulWidget {
-  const homePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<homePage> createState() => _homePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _homePageState extends State<homePage> {
+class _HomePageState extends State<HomePage> {
+  void _refresh() {
+    setState(() {});
+  }
+
+  void _showBottomSheet(TaskModel task) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "Task",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            ListTile(
+              title: const Text("Delete"),
+              onTap: () async {
+                try {
+                  await deleteTask(task.id);
+                  Navigator.pop(context);
+                  _refresh();
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(const SnackBar(content: Text("Task Deleted")));
+                } catch (error) {
+                  throw Exception("error occur in deleting: $error");
+                }
+              },
+            ),
+            ListTile(
+              title: const Text("Cancel"),
+              onTap: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("HomePage")),
+      appBar: AppBar(title: const Text("Home Page")),
+      body: FutureBuilder<List<TaskModel>>(
+        future: fetchTasks(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text("No tasks available"));
+          }
+
+          final tasks = snapshot.data!;
+          return ListView.builder(
+            itemCount: tasks.length,
+            itemBuilder: (context, index) {
+              final task = tasks[index];
+              return GestureDetector(
+                onLongPress: () => _showBottomSheet(task),
+                child: Card(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 8,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Task: ${task.task_name}'),
+                        Text('Type: ${task.task_type}'),
+                        Text('Time: ${task.time_details}'),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
